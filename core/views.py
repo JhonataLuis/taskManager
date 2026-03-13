b7ed4b646500881b13013cbb97c4ea8a281c9d5b
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-13 14:23:59 -0300

    criado função para editar uma tarefa
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
@@ -23,5 +23,23 @@ def salvar(request):
         )
     tarefas = Task.objects.all()
     return render(request, "index.html", {"tarefas": tarefas})
+
+def editar(request, id):
+    task = Task.objects.get(id=id)
+    return render(request, "update.html", {"task": task})
+
+def update(request, id):
+    task = Task.objects.get(id=id)
+
+    if request.method == "POST":
+        task.titulo = request.POST.get("titulo")
+        task.descricao = request.POST.get("descrição")
+        task.criado_em = request.POST.get("criado_em")
+        task.vence_em = request.POST.get("vence_em")
+        task.prioridade = request.POST.get("prioridade")
+        task.concluida = request.POST.get("concluida") == "on"
+        task.save()
+
+        return redirect()
 
 
