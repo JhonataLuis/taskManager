7557cecabb779dd4d29970c5ca406845156ecd59
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-13 11:11:13 -0300

    criado tabela db através do models.py
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Baixa'),
+        (2, 'Média'),
+        (3, 'Alta'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    descricao = models.CharField(blank=True)
+
+    # Datas importantes
+    criado_em = models.DateTimeField(auto_now_add=True)
+    vence_em = models.DateTimeField(null=True, blank=True)
+
+    # Status
+    concluida = models.BooleanField(default=False)
+    prioridade = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+
+def __srt__(self):
+    return self.titulo, self.descricao
+
+class Meta:
+    ordering = ['-prioridade', 'vence_em']
