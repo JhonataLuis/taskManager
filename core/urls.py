e27201e8ef2a6870975ed1961f421a0683bf126d
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-13 12:46:30 -0300

    criado função cadastrar tarefa
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, salvar
 
 urlpatterns = [
     path('', home),
+    path('salvar/', salvar, name="salvar")
 
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,23 @@ from .models import Task
 def home(request):
     tarefas = Task.objects.all()
     return render(request, "index.html", {"tarefas": tarefas})
+
+def salvar(request):
+    titulo = request.POST.get("titulo")
+    descricao = request.POST.get("descricao")
+    criado_em = request.POST.get("criado_em")
+    vence_em = request.POST.get("vence_em")
+    concluida = request.POST.get("concluida") == "on" # Checkbox retorna 'on' quando marcado
+    prioridade = request.POST.get("prioridade")
+    Task.objects.create(
+        titulo=titulo, 
+        descricao=descricao,
+        criado_em=criado_em,
+        vence_em=vence_em,
+        concluida=concluida,
+        prioridade=prioridade
+        )
+    tarefas = Task.objects.all()
+    return render(request, "index.html", {"tarefas": tarefas})
+
+
